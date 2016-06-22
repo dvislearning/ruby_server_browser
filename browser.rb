a0c5f20f6df05e922d32f0fa6d20837a06e1ce79
DV <dvislearning@gmail.com>
2016-06-22 00:19:08 -0700

    add response to get request

--- browser.rb
+++ browser.rb
@@ -3,8 +3,8 @@ require 'json'
  
 host = 'localhost'     # The web server
 port = 2000                           # Default HTTP port
-path = "thanks.html"                 # The file we want 
-
+post_path = "thanks.html"                 # The file we want 
+get_path = "hello.html"
 
 puts "Which method would you like to use?"
 method = gets.chomp.upcase
@@ -20,9 +20,9 @@ if method == 'POST'
   email = gets.chomp
   content[:viking] = {:name => name, :email => email}
   content = content.to_json
-  request = "#{method} #{path} HTTP/1.0" + "\r\n" + "Content_length: #{content.length}" + "\r\n\r\n" + "#{content}" + "\r\n" + "END"  
+  request = "#{method} #{post_path} HTTP/1.0" + "\r\n" + "Content_length: #{content.length}" + "\r\n\r\n" + "#{content}" + "\r\n" + "END"  
 else
-  request = "#{method} #{path} HTTP/1.0" + "END"
+  request = "#{method} #{get_path} HTTP/1.0" + "END"
 end
 
 # This is the HTTP request we send to fetch a file
